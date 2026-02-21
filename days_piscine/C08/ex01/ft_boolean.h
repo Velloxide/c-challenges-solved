0fbe12784a1406c495d12ed3242437d42d8d087b
said boutayeb <saidmoney115@gmail.com>
2026-02-21 23:21:49 +0000

    V158

---
+++ days_piscine/C08/ex01/ft_boolean.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Itachi-Logic <ILogic@student.1337.ma>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/20 23:25:38 by Itachi-Logic      #+#    #+#             */
+/*   Updated: 2026/02/21 00:25:22 by Itachi-Logic     ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+# include <unistd.h>
+
+typedef int	t_bool;
+
+# define EVEN (nbr) (nbr % 2 == 0)
+# define TRUE 1
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG  "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+#endif
