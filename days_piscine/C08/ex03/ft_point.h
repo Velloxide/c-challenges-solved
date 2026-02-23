857385929e07d7625c269d78557169645eb8b4d2
said boutayeb <saidmoney115@gmail.com>
2026-02-23 00:30:08 +0000

    V160

--- days_piscine/C08/ex03/ft_point.h
+++ days_piscine/C08/ex03/ft_point.h
@@ -6,8 +6,17 @@
 /*   By: Itachi-Logic <ILogic@student.1337.ma>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/22 00:55:22 by Itachi-Logic      #+#    #+#             */
-/*   Updated: 2026/02/22 00:55:23 by Itachi-Logic     ###   ########.fr       */
+/*   Updated: 2026/02/23 00:28:26 by Itachi-Logic     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_POINT_H
+# define FT_POINT_H
 
+typedef struct ft_point
+{
+	int	x;
+	int	y;
+}	t_point;
+
+#endif
