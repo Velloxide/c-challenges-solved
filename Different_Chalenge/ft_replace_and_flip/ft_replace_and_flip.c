c7dbb07254e5f157e9cab68c4fb538ef5e9bfe1e
said boutayeb <saidmoney115@gmail.com>
2026-03-09 23:18:49 +0000

    V178

--- Different_Chalenge/ft_replace_and_flip/ft_replace_and_flip.c
+++ Different_Chalenge/ft_replace_and_flip/ft_replace_and_flip.c
@@ -6,7 +6,7 @@
 /*   By: Itachi-Logic <ILogic@student.1337.ma>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 00:06:41 by Itachi-Logic      #+#    #+#             */
-/*   Updated: 2026/03/09 23:11:55 by Itachi-Logic     ###   ########.fr       */
+/*   Updated: 2026/03/09 23:18:31 by Itachi-Logic     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@
 
 char	*ft_replace_and_flip(char *str)
 {
-
+	//$>./a.out "Run   go fast go  and lO jump go  here" | cat -e
+	//	     Run   Lo  fast Lo  and 11 go jump Lo   here \n$>
+	
 }
 
 int	main(int argc, char *argv[])
