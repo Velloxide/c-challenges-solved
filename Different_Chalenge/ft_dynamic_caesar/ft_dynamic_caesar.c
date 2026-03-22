e038b9c1336035104de722263cb5e85edc28b5d7
said boutayeb <saidmoney115@gmail.com>
2026-03-22 11:08:06 +0100

    V192

--- Different_Chalenge/ft_dynamic_caesar/ft_dynamic_caesar.c
+++ Different_Chalenge/ft_dynamic_caesar/ft_dynamic_caesar.c
@@ -6,7 +6,7 @@
 /*   By: Itachi-Logic <ILogic@student.1337.ma>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/08 01:32:56 by Itachi-Logic      #+#    #+#             */
-/*   Updated: 2026/03/22 10:47:09 by Itachi-Logic     ###   ########.fr       */
+/*   Updated: 2026/03/22 11:07:49 by Itachi-Logic     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,15 @@ char	*ft_dynamic_caesar(char *str, int shift)
 		return (NULL);
 	arry[i] = '\0';
 	i = 0;
+	shift = (shift % 26 + 26) % 26;
 	while (str[i])
 	{
-		arry[i] = (str[i] - 'a' + shift) % 26 + 'a';
+		if (str[i] >= 'a' && str[i] <= 'z')
+			arry[i] = (str[i] - 'a' + shift) % 26 + 'a';
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			arry[i] = (str[i] - 'A' + shift) % 26 + 'A';
+		else
+			arry[i] = str[i];
 		i++;
 	}
 	return (arry);
@@ -36,11 +42,11 @@ char	*ft_dynamic_caesar(char *str, int shift)
 
 int	main(int argc, char *argv[])
 {
-	if (argc != 3)
-		return (0);
-	int	nb;
+	int		nb;
 	char	*str;
 
+	if (argc != 3)
+		return (0);
 	nb = atoi(argv[2]);
 	str = ft_dynamic_caesar(argv[1], nb);
 	printf("%s\n", str);
