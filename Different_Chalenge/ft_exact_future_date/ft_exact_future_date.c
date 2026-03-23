2ab5d1946c193387301371d60a5dcc621c8e6807
said boutayeb <saidmoney115@gmail.com>
2026-03-23 21:16:58 +0100

    V193

--- Different_Chalenge/ft_exact_future_date/ft_exact_future_date.c
+++ Different_Chalenge/ft_exact_future_date/ft_exact_future_date.c
@@ -6,7 +6,7 @@
 /*   By: Itachi-Logic <ILogic@student.1337.ma>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/23 15:31:11 by Itachi-Logic      #+#    #+#             */
-/*   Updated: 2026/03/23 19:12:17 by Itachi-Logic     ###   ########.fr       */
+/*   Updated: 2026/03/23 21:16:38 by Itachi-Logic     ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,25 +26,6 @@ int	ft_days_in_month(int month, int year)
 	return (31);
 }
 
-void	ft_full_str(char *str, int d, int m, int y, int len_year)
-{
-	int	i;
-
-	str[0] = (d / 10) + '0';
-	str[1] = (d % 10) + '0';
-	str[2] = '/';
-	str[3] = (m / 10) + '0';
-	str[4] = (m % 10) + '0';
-	str[5] = '/';
-	i = 5 + len_year;
-	while (i > 5)
-	{
-		str[i] = (y % 10) + '0';
-		y /= 10;
-		i--;
-	}
-}
-
 int	ft_numlen(int year)
 {
 	int	len;
@@ -58,43 +39,68 @@ int	ft_numlen(int year)
 	return (len);
 }
 
+void	ft_full_str(char *str, int d, int m, int y)
+{
+	int	i;
+
+	str[0] = (d / 10) + '0';
+	str[1] = (d % 10) + '0';
+	str[2] = '/';
+	str[3] = (m / 10) + '0';
+	str[4] = (m % 10) + '0';
+	str[5] = '/';
+	i = 5 + ft_numlen(y);
+	while (i > 5)
+	{
+		str[i] = (y % 10) + '0';
+		y /= 10;
+		i--;
+	}
+}
+
+int	ft_is_valid(int d, int m, int y, long long d_t_a)
+{
+	if (m < 1 || m > 12 || d_t_a < 0)
+		return (0);
+	if (d > ft_days_in_month(m, y) || d < 1)
+		return (0);
+	return (1);
+}
+
 char	*ft_exact_future_date(int day, int month, int year, long long days_to_add)
 {
 	char	*str;
-	int		len_year;
 
+	if (!ft_is_valid(day, month, year, days_to_add))
+		return (NULL);
 	year = year + ((days_to_add / 146097) * 400);
 	days_to_add = days_to_add % 146097;
-	while (days_to_add > 0)
+	while (days_to_add-- > 0)
 	{
-		days_to_add--;
-		day++;
-		if (day > ft_days_in_month(month, year))
+		if (++day > ft_days_in_month(month, year))
 		{
 			day = 1;
-			month++;
-			if (month > 12)
+			if (++month > 12)
 			{
 				year++;
 				month = 1;
 			}
 		}
 	}
-	len_year = ft_numlen(year);
-	str = malloc((7 + len_year) * sizeof(char));
+	str = malloc((7 + ft_numlen(year)) * sizeof(char));
 	if (!str)
 		return (NULL);
-	str[6 + len_year] = '\0';
-	ft_full_str(str , day, month, year, len_year);
+	str[6 + ft_numlen(year)] = '\0';
+	ft_full_str(str, day, month, year);
 	return (str);
 }
 
 int	main(int argc, char *argv[])
 {
-	char	*str;
-	int		day;
-	int		month;
-	int		year;
+	char		*str;
+	int			day;
+	int			month;
+	int			year;
 	long long	days_to_add;
 
 	if (argc != 5)
@@ -104,6 +110,9 @@ int	main(int argc, char *argv[])
 	year = atoi(argv[3]);
 	days_to_add = atoll(argv[4]);
 	str = ft_exact_future_date(day, month, year, days_to_add);
+	if (!str)
+		return (0);
 	printf("%s\n", str);
+	free(str);
 	return (0);
 }
