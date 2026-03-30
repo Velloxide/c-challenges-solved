e7fb0b3c00c3524a662c1ca31c5547092949e43d
said boutayeb <saidmoney115@gmail.com>
2026-03-30 21:06:49 +0100

    V196

---
+++ days_piscine/C09/ex01/includes/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Itachi-Logic <ILogic@student.1337.ma>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/30 17:37:19 by Itachi-Logic      #+#    #+#             */
+/*   Updated: 2026/03/30 19:55:33 by Itachi-Logic     ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+#define FT_H
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_swap(int *a, int *b);
+int	ft_strcmp(char *s1, char *s2);
+int	ft_strlen(char *str);
+
+#endif
